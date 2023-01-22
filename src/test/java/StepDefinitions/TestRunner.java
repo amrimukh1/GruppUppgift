a16dea0e0585f5bff2d955ed2f5a8ddcd87ca602
amrimukh1 <amrimukh1@gmail.com>
2023-01-22 12:34:04 +0100

    Added new file and made changes in existing one's

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"}, monochrome= true, 
-plugin= {"pretty","html:target/HTMLReports/report.html"})
+plugin= {"pretty","html:target/HTMLReports/report.html",
+		"json:target/JSONReports/report.json","junit:target/JunitReports/report.xml"})
 public class TestRunner {
 
 }
